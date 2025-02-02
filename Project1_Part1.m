218ef79a28bfa91a43414f60777b80228574aa9d
jpalmer0930 <j.palmer0930@tamu.edu>
2025-02-01 19:32:39 -0600

    Update Project1_Part1.m

--- Project1_Part1.m
+++ Project1_Part1.m
@@ -1,167 +1,72 @@
 % Initialization
-const_torque = 1; % Torque input in Nm
-J = 1;  % Inertia in kg m^2
-b = 1; % damping coefficient Nm/(rad/s)
-
-% Initial conditions
-omega0 = 0;  % rad/s
+omega0_values = [10 0];  % rad/s
 theta0 = 0;  % rad
+J_values = [100 0.01];  % Inertia in kg m^2
+b_values = [10 0.1]; % damping coefficient Nm/(rad/s)
 
-%set_param('Project1_shaft/Simple Shaft/IntegrateAcceleration', 'Integrator', '3')
+const_torque_values = [0, 100]; % Torque input in Nm
+w_values = [0.1 100]; %frequency
 
-tout = 25;
-dt = [1 0.1 0.001];
+dt_values = [1 0.1 0.001];
+int_method = ["ode1", "ode4", "ode45", "ode23tb"];
 
-% Define the function f
-f = @(t, y) (const_torque - b*y)/J;
+% Initialize arrays to store results
+cpu_time = zeros((length(const_torque_values)+length(w_values)), length(J_values), length(b_values), length(omega0_values), length(dt_values));
+max_error = zeros((length(const_torque_values)+length(w_values)), length(J_values), length(b_values), length(omega0_values), length(dt_values));
 
-% Euler method
-cpu_time1 = [];
-max_error1 = [];
-h = 1;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1
-    y(k+1) = y(k) + h * f(t(k), y(k));
+% Nested loops to iterate through all combinations
+for i = 1:(length(const_torque_values)+length(w_values))
+    for j = 1:length(J_values)
+        for k = 1:length(b_values)
+            for l = 1:length(omega0_values)
+                for m = 1:length(dt_values)
+                    for n = 1:length(int_method)
+                        if i <= 2
+                            const_torque = const_torque_values(i);
+                            w = 0;
+                        else
+                            w = w_values(i-2);
+                            const_torque = 0;
+                        end
+                        J = J_values(j);
+                        b = b_values(k);
+                        omega0 = omega0_values(l);
+                        dt = dt_values(m);
+                        
+                        % Set simulation parameters
+                        set_param(x1_SimpleShaft, 'StopTime', '25');
+                        if n <= 2
+                            set_param(x1_SimpleShaft, 'SolverType', 'Fixed-step');
+                            set_param(x1_SimpleShaft, 'FixedStep', num2str(dt));
+                            set_param(x1_SimpleShaft, 'SolverName', int_method(n));
+                        else
+                            set_param(x1_SimpleShaft, 'SolverType', 'Variable-step');
+                            set_param(x1_SimpleShaft, 'SolverName', int_method(n));
+                        end
+                        
+                        % Run simulation and measure CPU time
+                        tStart = cputime;
+                        sim("x1_SimpleShaft");
+                        tEnd = cputime - tStart;
+                        cpu_time(i,j,k,l,m) = tEnd;
+                        
+                        % Calculate theoretical solution
+                        t = 0:dt:25;
+                        w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
+                        
+                        % Calculate maximum error
+                        max_error(i,j,k,l,m) = max(abs(omega(1) - transpose(w_theoretical)));
+                        
+                    end
+                end
+            end
+        end
+    end
 end
-tEnd = cputime - tStart;
 
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
+% Close the model
+close_system(x1_SimpleShaft, 0);
 
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time1(end+1) = tEnd;
-max_error1(end+1) = max_error;
-% Damping Force
-F_b = b*y;                  % <----- IS THIS BEING USED
-
-h = 0.1;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1;
-    y(k+1) = y(k) + h * f(t(k), y(k));
-end
-tEnd = cputime - tStart;
-disp(['Time: ', num2str(tEnd)]);
-
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
-
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time1(end+1) = tEnd;
-max_error1(end+1) = max_error;
-
-h = 0.001;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1
-    y(k+1) = y(k) + h * f(t(k), y(k));
-end
-tEnd = cputime - tStart;
-
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
-
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time1(end+1) = tEnd;
-max_error1(end+1) = max_error;
-F_b = b*y;
-
-% Runge-Kutta method
-cpu_time2 = [];
-max_error2 = [];
-h = 1;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1
-    k1 = f(t(k), y(k));
-    k2 = f(t(k) + 0.5*h, y(k) + 0.5*h*k1);
-    k3 = f(t(k) + 0.5*h, y(k) + 0.5*h*k2);
-    k4 = f(t(k) + h, y(k) + h*k3);
-
-    y(k+1) = y(k) + (1/6) * (k1 + 2*k2 + 2*k3 + k4) * h;
-    tEnd = cputime - tStart;
-end
-%tEnd = cputime - tStart;
-
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
-
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time2(end+1) = tEnd;
-max_error2(end+1) = max_error;
-F_b = b*y;
-
-h = 0.1;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1
-    k1 = f(t(k), y(k));
-    k2 = f(t(k) + 0.5*h, y(k) + 0.5*h*k1);
-    k3 = f(t(k) + 0.5*h, y(k) + 0.5*h*k2);
-    k4 = f(t(k) + h, y(k) + h*k3);
-
-    y(k+1) = y(k) + (1/6) * (k1 + 2*k2 + 2*k3 + k4) * h;
-end
-tEnd = cputime - tStart;
-
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
-
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time2(end+1) = tEnd;
-max_error2(end+1) = max_error;
-F_b = b*y;
-
-h = 0.001;
-t = 0:h:tout;
-y = zeros(size(t));
-y(1) = omega0;
-
-tStart = cputime;
-for k = 1:length(t)-1
-    k1 = f(t(k), y(k));
-    k2 = f(t(k) + 0.5*h, y(k) + 0.5*h*k1);
-    k3 = f(t(k) + 0.5*h, y(k) + 0.5*h*k2);
-    k4 = f(t(k) + h, y(k) + h*k3);
-
-    y(k+1) = y(k) + (1/6) * (k1 + 2*k2 + 2*k3 + k4) * h;
-end
-tEnd = cputime - tStart;
-
-% Calculate theoretical solution for comparison (for step input)
-w_theoretical = omega0 * exp(-b*t/J) + (const_torque/b) * (1 - exp(-b*t/J));
-
-% Calculate maximum error
-max_error = max(abs(y - w_theoretical));
-
-cpu_time2(end+1) = tEnd;
-max_error2(end+1) = max_error;
 F_b = b*y;
 
 figure;
@@ -193,49 +98,3 @@ hold on
 plot(cpu_time2, max_error2, '-o');
 legend('Euler', 'RK4');
 hold off
-
-
-%Contour Plots (Don't work right now because of the singular inputs)
-% figure;
-% [X, Y] = meshgrid(cpu_time1, max_error1);
-% [C, h] = contour(X, Y, -b_values./J_values);
-% clabel(C, h);
-% xlabel('CPU Time (s)');
-% ylabel('Max Simulation Error');
-% title('Contour Plot of System Eigenvalues for Euler Method');
-% colorbar;
-% clim([min(-b_values./J_values, [], 'all') max(-b_values./J_values, [], 'all')]);
-% colormap('jet');
-% 
-% figure;
-% [X, Y] = meshgrid(cpu_time1, max_error1);
-% [C, h] = contour(X, Y, omega0);
-% clabel(C, h);
-% xlabel('CPU Time (s)');
-% ylabel('Max Simulation Error');
-% title('Contour Plot of Input Frequencies for Euler Method');
-% colorbar;
-% clim([min(omega0, [], 'all') max(omega0, [], 'all')]);
-% colormap('jet');
-% 
-% figure;
-% [X, Y] = meshgrid(cpu_time2, max_error2);
-% [C, h] = contour(X, Y, -b_values./J_values);
-% clabel(C, h);
-% xlabel('CPU Time (s)');
-% ylabel('Max Simulation Error');
-% title('Contour Plot of System Eigenvalues for RK4 Method');
-% colorbar;
-% clim([min(-b./J, [], 'all') max(-b./J, [], 'all')]);
-% colormap('jet');
-% 
-% figure;
-% [X, Y] = meshgrid(cpu_time2, max_error2);
-% [C, h] = contour(X, Y, omega0);
-% clabel(C, h);
-% xlabel('CPU Time (s)');
-% ylabel('Max Simulation Error');
-% title('Contour Plot of Input Frequencies for RK4 Method');
-% colorbar;
-% clim([min(omega0, [], 'all') max(omega0, [], 'all')]);
-% colormap('jet');
