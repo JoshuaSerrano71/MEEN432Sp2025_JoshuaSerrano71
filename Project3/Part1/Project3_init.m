395448a855e122f92cf8bef376b13225baa24366
jpalmer0930 <j.palmer0930@tamu.edu>
2025-03-02 22:13:12 -0600

    Update Project3_init.m

--- Project3/Part1/Project3_init.m
+++ Project3/Part1/Project3_init.m
@@ -1,3 +1,5 @@
+clear
+
 % Project 3 Initilization
 r = 0.3;
 m = 1000;
@@ -5,6 +7,12 @@ I = 1600;
 I_w = 0;
 F_drag = 0;
 
+% Importing speed values
+init_highway_epa
+init_urban_epa
+
+% Running simulink
+simout = sim("MEEN432_Project3_start");
 
 %% plotting epa cycle (urban)
 f = figure;
@@ -13,4 +21,8 @@ hold on
 plot(time, urban_epa + 3, '--b');
 plot(time, urban_epa - 3, '--b');
 f.WindowState = 'maximized';
-plot(out.simv, 'g');
+plot(simout.simv, 'g');
+title('Simulated Vehicle Velocity vs Time')
+ylabel('Velocity (mph)')
+xlabel('Time (s)')
+legend('Drive Cycle Velocity', '3 mph Error Band', '', 'Sim Velocity');
