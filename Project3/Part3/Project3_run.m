ad39c340b77fa073a3e43b5e32ae2a6a8e627237
jpalmer0930 <j.palmer0930@tamu.edu>
2025-03-29 22:03:48 -0500

    Project 3 Week 3 Updates

---
+++ Project3/Part3/Project3_run.m
@@ -0,0 +1,32 @@
+clear
+
+% Project 3 Initilization
+p3_init
+
+% Importing speed values
+init_highway_epa
+init_urban_epa_2cycle % CHANGE THIS FOR DIFFERENT EPA CYCLE
+
+%% Model Init - CHANGE THIS FOR DIFFERENT SETTING
+epa = urban_epa; % highway_epa
+time = urban_time; % highway_time
+
+%% Running simulink
+set_param("MEEN432_Project3_Wk3_Model", 'StopTime', string(length(urban_epa)))
+simout = sim("MEEN432_Project3_Wk3_Model");
+
+total_power = sum(simout.MotorPower.Data);
+
+%% plotting epa cycle (urban)
+f = figure;
+plot(time, epa, 'r');
+hold on
+plot(time, epa + 3, '--b');
+plot(time, epa - 3, '--b');
+f.WindowState = 'maximized';
+plot(simout.veh_speed, 'g');
+title('Simulated Vehicle Velocity vs Time')
+ylabel('Velocity (mph)')
+xlabel('Time (s)')
+legend('Drive Cycle Velocity', '3 mph Error Band', '', 'Sim Velocity');
+hold off
